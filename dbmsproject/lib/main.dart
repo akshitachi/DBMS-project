fda6b13e0fdf7363e0582c7854aafdf0a454f67b
Akshil <shahakshil2001@gmail.com>
2022-03-22 20:11:23 +0530

    small change

--- dbmsproject/lib/main.dart
+++ dbmsproject/lib/main.dart
@@ -30,6 +30,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
+      debugShowCheckedModeBanner: false,
       home: AuthChecker(),
     );
   }
